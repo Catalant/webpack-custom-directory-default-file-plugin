d376aeb3209cd35edb6cbf56ca98f7d0470cc153
Misha Koryak <mkoryak@gmail.com>
2015-11-12 11:17:27 -0500

    move useful functionality into the plugin

--- README.md
+++ README.md
@@ -11,7 +11,49 @@ in order to require:
 `lib/ReallyImportantModule/whatever_you_want_goes_here.js`
 
 
-This module is designed to be used as strategy for a `webpack.ResolverPlugin` like so:
+This module is designed to be used as strategy for a `webpack.ResolverPlugin`.
+
+It comes with a prebuilt resolver called 'regexp' that let you specify any index file with a regexp.
+
+### DirectoryDefaultFilePlugin.resolver.regexp
+
+This resolver can be used to make any filename that matches indexRegexp be the index file of that directory.
+There can only ever be 0 or 1 such files per dir, if there is more than one, the build will fail (unless ignoreMultpleMatches is true)
+
+includeDirRegexp can be used to limit the dirs where this behavior is used. 
+The default value for this param will include everything under the current working dir except anything in node_modules.
+
+Options object contains:
+     @param indexRegexp
+     @param includeDirRegexp
+     @param ignoreMultipleMatches
+
+Example use
+
+```js
+var CustomDefaultFilePlugin = require('webpack-custom-directory-default-file-plugin');
+
+plugins: [
+     new webpack.ResolverPlugin([
+         // this will use any file ending in .index.js as the index file for that dir
+         // unless there is an index.js file there already.
+         new CustomDefaultFilePlugin(CustomDefaultFilePlugin.resolver.regexp(
+             // 'index' file must match this
+             /\.index.js$/,
+
+             // directory where that file lives must match this (has sane default)
+             new RegExp("^(" + process.cwd() + "/(?!node_modules)|" + process.cwd() + "/node_modules/my_other_module)"))
+         )
+     ]),
+
+```
+
+### Writing your own resolver
+
+The plugin takes a fn with 3 args: resolver, req, done (the first 2 are webpack objects with useful stuff on them).
+
+The  `done` callback must be called with undefined to fallback on default webpack behavior,
+or full path to the file that will be the index file.
 
 ```js
 var CustomDefaultFilePlugin = require('webpack-custom-directory-default-file-plugin');
@@ -22,15 +64,12 @@ var webpackConfig = {
 
   plugins: [
     new webpack.ResolverPlugin([
-        new CustomDefaultFilePlugin(function(resolver, req){
+        new CustomDefaultFilePlugin(function(resolver, req, done){
             var directory = resolver.join(req.path, req.request);
             var files = glob.sync(directory+'/*.idx.js');
-            if(files.length > 2){
-                throw "multiple index files in dir:"+directory+" files:"+files+" -- this is bad. you should only have one or none!";
-            }
             if(files.length == 1){
                 // important: remove ext from path!
-                return files[0].replace(/\.\w+$/, '');
+                return done(files[0].replace(/\.\w+$/, ''));
             }
         })
     ]),
@@ -40,3 +79,4 @@ var webpackConfig = {
 ```
 
 The example above lets you `require('lib/ReallyImportantModule')` and it will load `lib/ReallyImportantModule/anything_goes_here.idx.js`
+
--- index.js
+++ index.js
@@ -1,37 +1,97 @@
-/* jshint node:true, esnext:true */
+var path = require('path');
+var fs = require('fs');
 
-/*****
- * Modified clone of https://github.com/webpack/enhanced-resolve/blob/3b88905ad3cb6392f48f7bc57d351891774fd93b/lib/DirectoryDefaultFilePlugin.js
- *****
-
-/*
-  MIT License http://www.opensource.org/licenses/mit-license.php
-  Author Tobias Koppers @sokra
-*/
-var basename = require('path').basename;
-
-function DirectoryDefaultFilePlugin(cb) {
-    this.cb = cb;
+function DirectoryDefaultFilePlugin(resolverFn) {
+    this.resolverFn = resolverFn || function(){
+        throw new Error("DirectoryDefaultFilePlugin: resolverFn expected");
+    };
 }
-module.exports = DirectoryDefaultFilePlugin;
 
 DirectoryDefaultFilePlugin.prototype.apply = function (resolver) {
-  var cb = this.cb || function(){ throw "callback not defined!" };
-  resolver.plugin('directory', function (req, done) {
-    var directory = resolver.join(req.path, req.request);
+    var resolverFn = this.resolverFn;
+    resolver.plugin('directory', function (req, done) {
+        var directory = resolver.join(req.path, req.request);
 
-    resolver.fileSystem.stat(directory, function (err, stat) {
-      if (err || !stat) return done();
-      if (!stat.isDirectory()) return done();
+        resolver.fileSystem.stat(directory, function (err, stat) {
+            if (err || !stat) return done();
+            if (!stat.isDirectory()) return done();
 
-      resolver.doResolve('file', {
-        path: req.path,
-        query: req.query,
-        request: cb(resolver, req)
-      }, function (err, result) {
-        return done(undefined, result || undefined);
-      });
+            resolverFn(resolver, req, function(filename) {
+                if(filename && filename.substring(0, directory.length) != directory) {
+                    filename = path.join(directory, filename);
+                }
+                resolver.doResolve('file', {
+                    path: req.path,
+                    query: req.query,
+                    request: filename
+                }, function (err, result) {
+                    return done(undefined, result || undefined);
+                });
+            });
+        });
+
     });
-
-  });
 };
+
+DirectoryDefaultFilePlugin.resolver = {
+    /**
+     * This resolver can be used to make any filename that matches indexRegexp be the index file of that directory.
+     * There can only ever be 0 or 1 such files per dir, if there is more than one, the build will fail (unless ignoreMultipleMatches is true)
+     *
+     * includeDirRegexp can be used to limit the dirs where this behavior is used.
+     * The default value for this param will include everything under the current working dir except anything in node_modules.
+     *
+     * Example use
+     *
+     * plugins: [
+         new webpack.ResolverPlugin([
+             new CustomDefaultFilePlugin(CustomDefaultFilePlugin.resolver.regexp(
+                 /\.index.js$/,
+                 new RegExp("^(" + process.cwd() + "/(?!node_modules)|" + process.cwd() + "/node_modules/my_other_module)"))
+             )
+         ]),
+     *
+     * this will use any file ending in .index.js as the index file for that dir, unless there is an index.js file there already.
+     *
+     * Options object contains:
+     *      @param indexRegexp - thing used as index's filename matches this
+     *      @param includeDirRegexp - dir where the file is in matches this
+     *      @param ignoreMultipleMatches - if there are multiple files that match regexp in dir, dont throw error, ignore them all.
+     * @returns {Function}
+     */
+    regexp: function(options){
+        var indexRegexp = options.indexRegexp;
+        var includeDirRegexp = options.includeDirRegexp || new RegExp("^" + process.cwd() + "/(?!node_modules)");
+        var ignoreMultipleMatches = options.ignoreMultipleMatches || false;
+        return function(resolver, req, done) {
+            var directory = resolver.join(req.path, req.request);
+            if (directory.match(includeDirRegexp)) {
+                fs.readdir(directory, function (err, files) {
+                    var indexFiles = files.filter(function (f) {
+                        // if we have an index.js in the dir, fallback to regular behavior
+                        return f == "index.js";
+                    });
+                    if (indexFiles.length == 0) {
+                        indexFiles = files.filter(function (f) {
+                            // now lets see if any left over files match our regexp
+                            return f.match(indexRegexp);
+                        });
+                        if (!ignoreMultipleMatches && indexFiles.length > 1) {
+                            throw new Error("CustomDefaultFilePlugin: Multiple index files in dir: " +
+                            directory + "\nFiles: [" + indexFiles + "]\nMatching regexp: " +
+                            indexRegexp.source + "\n(should have at most one such file!!!!)\n\n");
+                        } else if (indexFiles.length == 1) {
+                            //must chop off extension!! this is what webpack expects.
+                            return done(indexFiles[0].replace(/\.[^.]+$/, ''));
+                        }
+                    }
+                    return done();
+                });
+            } else {
+                done();
+            }
+        }
+    }
+};
+
+module.exports = DirectoryDefaultFilePlugin;
